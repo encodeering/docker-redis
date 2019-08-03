6efccf816b3e5be23fcfbf207a561cf5d5d7fd9d
Michael Clausen <encodeering@gmail.com>
2019-08-03 10:22:40 +0200

    updating scm repository to latest revision
    * 3193aad4ff9f2ea482a57d4a296b6541237f682c

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/debian-$ARCH:stretch" "debian:stretch-slim"
+docker-pull "$REPOSITORY/debian-$ARCH:buster" "debian:buster-slim"
 
 docker-build "$PROJECT/$VERSION"
