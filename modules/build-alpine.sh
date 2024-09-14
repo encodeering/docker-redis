e2a4342f358c5edef009e1c6f19289b955cb9d9c
Michael Clausen <encodeering@gmail.com>
2024-09-14 09:08:09 +0200

    updates postgres to the latest version
    * https://github.com/redis/docker-library-redis/commit/a8b4897fc92238fceaa137ce8a7391e3f1bd04b7

--- modules/build-alpine.sh
+++ modules/build-alpine.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.11" "alpine:3.11"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.20" "alpine:3.20"
 
 docker-build "$PROJECT/$VERSION/alpine"
--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/debian-$ARCH:buster" "debian:buster-slim"
+docker-pull "$REPOSITORY/debian-$ARCH:bookworm-slim" "debian:bookworm-slim"
 
-docker-build "$PROJECT/$VERSION"
+docker-build "$PROJECT/$VERSION/debian"
